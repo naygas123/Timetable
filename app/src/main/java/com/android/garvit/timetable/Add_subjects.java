afa067e81b594c2e0e74443e568cfc334fd45f63
Daiivol <daiivol.person@gmail.com>
2018-05-10 19:30:56 +0400

    Second commit
    Added new toolbar in add_subjects with better layout
    
    and changed all icons to better icons

--- app/src/main/java/com/android/garvit/timetable/Add_subjects.java
+++ app/src/main/java/com/android/garvit/timetable/Add_subjects.java
@@ -6,16 +6,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Menu;
 
 public class Add_subjects extends AppCompatActivity {
-
+    private android.support.v7.widget.Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_subjects);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.add_subject_toolbar);
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        inittoolbar();
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +24,29 @@ public class Add_subjects extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+    }
+
+    void inittoolbar(){
+        toolbar = findViewById(R.id.add_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        android.view.MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(android.view.MenuItem item){
+        if(item.getItemId()== R.id.save_subject){
+            // HERE ADD METHOD TO SAVE VALUES FROM EDIT TEXTS
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
